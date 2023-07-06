c5a749fd1310632a8ab349493664ffd1d9be7feb
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-07-06 17:36:52 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const menu = document.getElementById('burger-menu');
+const closed = document.getElementById('menu-close');
+const open = document.getElementById('open-burger');
+open.addEventListener('click', () => {
+  menu.classList.add('open')
+})
+
+closed.addEventListener('click', () => {
+  menu.classList.remove('open')
+})
