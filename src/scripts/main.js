31ec803a472a9ed8d775734352a581f48cf417d0
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2024-02-19 12:28:02 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,41 @@
-// const html = document.querySelector('.html');
-// const switch_theme = document.querySelector('.slider');
-// let counter = 1;
+'use strict';
 
-// switch_theme.addEventListener('click', () => {
+let counter = 0;
 
-//   if (counter%2 !== 0) {
-//     html.classList.remove('theme-light');
-//     html.className += ' theme-dark';
-//     counter++;
-//   } else {
-//     html.classList.remove('theme-dark');
-//     html.className += ' theme-light';
-//     counter++;
-//   }
-// });
+for (const menu of document.querySelectorAll(".content-menu")) {
+  menu.addEventListener("click", () => {
+      menu.querySelector(".menu-text").classList.toggle("menu-text-open");
+      menu.querySelector(".list_titles").classList.toggle("active_menu");
+      if (counter % 2 === 0) {
+        menu.classList.add("active");
+      } else {
+        menu.classList.remove("active");
+      }
+      counter = counter + 1;
+      menu.querySelector(".menu-content").classList.toggle(
+          "menu-content-hidden"
+      );
+      console.log(counter)
+  });
+}
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const select = dropdown.querySelector('.select');
+  const caret = dropdown.querySelector('.caret');
+  const menu = dropdown.querySelector('.menu');
+  const options = dropdown.querySelectorAll('.menu li');
+
+  select.addEventListener('click', () => {
+    select.classList.toggle('select-clicked');
+    menu.classList.toggle('menu-open');
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      select.classList.remove('select-clicked');
+      menu.classList.remove('menu-open');
+    });
+  });
+});
