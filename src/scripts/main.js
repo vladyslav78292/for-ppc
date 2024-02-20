620cca3fe9005c48349d192813841e4b232202fc
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2024-02-20 11:55:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -39,3 +39,16 @@ dropdowns.forEach(dropdown => {
     });
   });
 });
+
+const openMobileMenuBtn = document.querySelector(".device-menu");
+const headerMenu = document.querySelector(".header-nav");
+
+openMobileMenuBtn.addEventListener("click", () => {
+  if(openMobileMenuBtn.classList.contains("open")) {
+    openMobileMenuBtn.classList.remove("open");
+    headerMenu.classList.remove("active");
+  } else {
+    headerMenu.classList.add("active");
+    openMobileMenuBtn.classList.add("open");
+  }
+})
